7e5e39de04693bb53f2f4b0638ce420ec0d37d8f
becose <bennettconsultingservices@gmail.com>
2016-04-01 08:07:11 -0400

    Update BOCCIS.java

--- src/boccis/BOCCIS.java
+++ src/boccis/BOCCIS.java
@@ -15,7 +15,8 @@ public class BOCCIS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        mainmenuF mmf = new mainmenuF();
+        mmf.setVisible(true);
     }
     
 }
